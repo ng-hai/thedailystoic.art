abd35196abf6f576914435afd49c8500cfe7f83b
ng-hai <nghai.dev@gmail.com>
2023-12-22 03:05:07 +0700

    Integrate with MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
-
 import vercel from "@astrojs/vercel/serverless"
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   output: "hybrid",
   adapter: vercel(),
   markdown: {
-    remarkRehype: { footnoteLabelTagName: "h3" },
+    remarkRehype: {
+      footnoteLabelTagName: "h3",
+    },
   },
 })
